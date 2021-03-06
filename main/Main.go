fca7d1a2974aeb2a0507ee7d1e937614dc53e688
Slawomir Biernacki <slawomir.biernacki@hotmail.com>
2021-03-06 16:20:55 +0000

    Code cleanup

--- main/Main.go
+++ main/Main.go
@@ -5,60 +5,92 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	seldonapi "github.com/seldonio/seldon-core/operator/apis/machinelearning.seldon.io/v1"
+	seldonclientset "github.com/seldonio/seldon-core/operator/client/machinelearning.seldon.io/v1/clientset/versioned"
+	informer "github.com/seldonio/seldon-core/operator/client/machinelearning.seldon.io/v1/informers/externalversions"
 	"io/ioutil"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	"k8s.io/client-go/rest"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"os"
 	"path/filepath"
-	//"gopkg.in/yaml.v3"
+	"time"
 
-	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-	//deploymentutil "k8s.io/kubernetes/pkg/controller/deployment/util"
+	//machinelearningv1 "github.com/seldonio/seldon-core/operator/client/machinelearning.seldon.io/v1/clientset/versioned/typed/machinelearning.seldon.io/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
-func main() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+var clientset *seldonclientset.Clientset
 
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	//// create the clientset
-	//clientset, err := kubernetes.NewForConfig(config)
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//
-	//pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//
-	//for _, svc:=range pods.Items{
-	//	fmt.Fprintf(os.Stdout, "service name: %v\n", svc.Name)
-	//}
-	//
-	//fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-	//main2.Test(config)
-	create(config)
+func init() {
+	clientset, _ = GetSeldonClientSet()
 }
 
-func create(config *rest.Config) {
+func main() {
+	namespace := "test-aaa"
+
+	factory := informer.NewSharedInformerFactoryWithOptions(clientset, time.Second, informer.WithNamespace(namespace))
+	events := make(chan struct{})
+
+	informerr := factory.Machinelearning().V1().SeldonDeployments().Informer()
+	defer close(events)
+
+	go runSeldonCRDInformer(events, informerr, namespace)
+
+	deployment := create(namespace)
+	name := deployment.GetName()
+
+	err := waitForDeployment(name, namespace, time.Second, 100*time.Second)
+	if err != nil {
+		panic(err)
+	}
+
+	get, err := GetSeldonDeployment(deployment.GetName(), namespace)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("status %s.\n", get.Status.State)
+
+	err = DeleteSeldonDeployment(deployment.GetName(), namespace)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func runSeldonCRDInformer(stopCh <-chan struct{}, s cache.SharedIndexInformer, namespace string) {
+	handlers := cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+
+			//d := &seldonapi.SeldonDeployment{}
+			//// try following https://erwinvaneyk.nl/kubernetes-unstructured-to-typed/
+			//err := runtime.DefaultUnstructuredConverter.
+			//	FromUnstructured(obj.(*unstructured.Unstructured).UnstructuredContent(), d)
+			//if err != nil {
+			//	fmt.Println("could not convert obj to SeldonDeployment")
+			//	fmt.Print(err)
+			//	return
+			//}
+			fmt.Printf("Added! %s.\n", obj)
+
+			// do what we want with the SeldonDeployment/event
+		},
+		DeleteFunc: func(obj interface{}) {
+			// convert the obj as above do what we want with the SeldonDeployment/event
+			fmt.Printf("Deleted! %s.\n", obj)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			// convert the obj as above do what we want with the SeldonDeployment/event
+		},
+	}
+	s.AddEventHandler(handlers)
+	s.Run(stopCh)
+}
+
+func create(namespace string) *seldonapi.SeldonDeployment {
 	filename, _ := filepath.Abs("./main/test-resource.yaml")
 	yamlFile, err := ioutil.ReadFile(filename)
 
@@ -66,81 +98,32 @@ func create(config *rest.Config) {
 		panic(err)
 	}
 
-	namespace := "test-aaa"
-
 	var decUnstructured = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
 
-	deployment := &unstructured.Unstructured{}
+	d := &seldonapi.SeldonDeployment{}
+	//deployment := &unstructured.Unstructured{}
 	// Decode YAML to unstructured object.
-	if _, _, err := decUnstructured.Decode(yamlFile, nil, deployment); err != nil {
+	if _, _, err := decUnstructured.Decode(yamlFile, nil, d); err != nil {
 		panic(err)
 	}
 
-	//var dep seldon_protos.SeldonDeployment
-	//
-	//err = yaml.Unmarshal(yamlFile, &dep)
+	// try following https://erwinvaneyk.nl/kubernetes-unstructured-to-typed/
+	//err = runtime.DefaultUnstructuredConverter.
+	//	FromUnstructured(deployment.UnstructuredContent(), d)
 	//if err != nil {
-	//	panic(err)
+	//	fmt.Println("could not convert obj to SeldonDeployment")
+	//	fmt.Print(err)
+	//	return
 	//}
-	//
-	//fmt.Printf("Value: %#v\n", deployment)
 
-	client, err := dynamic.NewForConfig(config)
+	dep, err := CreateSeldonDeployment(d, namespace)
 	if err != nil {
 		panic(err)
 	}
 
-	deploymentRes := schema.GroupVersionResource{Group: "machinelearning.seldon.io", Version: "v1", Resource: "seldondeployments"}
+	fmt.Printf("Created deployment %q.\n", dep.GetName())
 
-	// Create Deployment
-	fmt.Println("Creating deployment...")
-	result, err := client.Resource(deploymentRes).Namespace("test-aaa").Create(context.TODO(), deployment, metav1.CreateOptions{})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Created deployment %q.\n", result.GetName())
-
-	status, found, err := unstructured.NestedStringMap(result.Object, "status")
-	fmt.Printf("status %q.\n", status)
-	fmt.Printf("found %q.\n", found)
-	//fmt.Printf("status %q.\n", result)
-
-	//clientset, err := kubernetes.NewForConfig(config)
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//
-	//deployments, err := clientset.AppsV1().Deployments("test-aaa").Get(context.TODO(), result.GetName(),  metav1.GetOptions{})
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-
-	// Delete Deployment
-
-	watchEvents(config)
-	prompt()
-
-	// List Deployments
-	fmt.Printf("Listing deployments in namespace ")
-	get, err := client.Resource(deploymentRes).Namespace(namespace).Get(context.TODO(), result.GetName(), metav1.GetOptions{})
-	if err != nil {
-		panic(err)
-	}
-	replicas, found, err := unstructured.NestedInt64(get.Object, "spec", "replicas")
-	if err != nil || !found {
-		fmt.Printf("Replicas found for deployment %s: error=%s", replicas, err)
-	}
-
-	prompt()
-
-	fmt.Println("Deleting deployment...")
-	deletePolicy := metav1.DeletePropagationForeground
-	if err := client.Resource(deploymentRes).Namespace("test-aaa").Delete(context.TODO(), result.GetName(), metav1.DeleteOptions{
-		PropagationPolicy: &deletePolicy,
-	}); err != nil {
-		panic(err)
-	}
-	fmt.Println("Deleted deployment.")
+	return dep
 }
 
 func prompt() {
@@ -155,72 +138,66 @@ func prompt() {
 	fmt.Println()
 }
 
-func watchEvents(config *rest.Config) {
-	//ctx, _ := context.WithCancel(context.Background()) //cancel!
-	////defer cancel()
-	//// create the clientset
-	//client, err := kubernetes.NewForConfig(config)
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//
-	//deploymentRes := schema.GroupVersionResource{Group: "machinelearning.seldon.io", Version: "v1", Resource: "seldondeployments"}
+func GetSeldonClientSet() (*seldonclientset.Clientset, error) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig2", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig2", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
 
-	// We will create an informer that writes added pods to a channel.
-	//pods := make(chan *v1.Pod, 1)
-	//informers := informers.NewSharedInformerFactory(client, 0)
-	//podInformer, err := informers.Core().V1().ForResource(deploymentRes)
-	//if err != nil {
-	//	panic(err.Error())
-	//}
-	//podInformer.Informer().AddEventHandler(&cache.ResourceEventHandlerFuncs{
-	//	AddFunc: func(obj interface{}) {
-	//		pod := obj.(*v1.Service)
-	//		fmt.Printf("pod added: %s/%s\n", pod.Namespace, pod.Name)
-	//		//pods <- pod
-	//	},
-	//})
-	//
-	//// Make sure informers are running.
-	//informers.Start(ctx.Done())
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
 
-	//prompt()
-
-	//for elem := range pods {
-	//	fmt.Println(elem)
-	//}
+	kubeClientset, err := seldonclientset.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return kubeClientset, nil
 }
 
-//func waitForDeploymentCompleteMaybeCheckRolling(c kubernetes.Clientset, d *apps.Deployment , pollInterval, pollTimeout time.Duration) error {
-//	var (
-//		deployment *apps.Deployment
-//		reason     string
-//	)
-//
-//	err := wait.PollImmediate(pollInterval, pollTimeout, func() (bool, error) {
-//		var err error
-//		deployment, err = c.AppsV1().Deployments("test-aaa").Get(context.TODO(), d.Name, metav1.GetOptions{})
-//		if err != nil {
-//			return false, err
-//		}
-//
-//		// If during a rolling update, make sure rolling update strategy isn't broken at any times.
-//
-//		// When the deployment status and its underlying resources reach the desired state, we're done
-//		if deploymentutil.DeploymentComplete(d, &deployment.Status) {
-//			return true, nil
-//		}
-//
-//		reason = fmt.Sprintf("deployment status: %#v", deployment.Status)
-//
-//		return false, nil
-//	})
-//
-//	if err == wait.ErrWaitTimeout {
-//		err = fmt.Errorf("%s", reason)
-//	}
-//	if err != nil {
-//		return fmt.Errorf("error waiting for deployment %q status to match expectation: %v", d.Name, err)
-//	}
-//	return nil
-//}
+func GetSeldonDeployment(name string, namespace string) (result *seldonapi.SeldonDeployment, err error) {
+	return clientset.MachinelearningV1().SeldonDeployments(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+}
+
+func DeleteSeldonDeployment(name string, namespace string) (err error) {
+	return clientset.MachinelearningV1().SeldonDeployments(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+}
+
+func CreateSeldonDeployment(deployment *seldonapi.SeldonDeployment, namespace string) (sdep *seldonapi.SeldonDeployment, err error) {
+	return clientset.MachinelearningV1().SeldonDeployments(namespace).Create(context.TODO(), deployment, metav1.CreateOptions{})
+}
+
+func waitForDeployment(name, namespace string, pollInterval, pollTimeout time.Duration) error {
+	var reason string
+	fmt.Print("Waiting")
+	err := wait.PollImmediate(pollInterval, pollTimeout, func() (bool, error) {
+		var err error
+		deployment, err := GetSeldonDeployment(name, namespace)
+		if err != nil {
+			return false, err
+		}
+
+		// When the deployment status and its underlying resources reach the desired state, we're done
+		if seldonapi.StatusStateAvailable == deployment.Status.State {
+			fmt.Print("\n")
+			return true, nil
+		}
+
+		reason = fmt.Sprintf("deployment status: %#v", deployment.Status)
+
+		fmt.Print(".")
+		return false, nil
+	})
+
+	if err == wait.ErrWaitTimeout {
+		err = fmt.Errorf("%s", reason)
+	}
+	if err != nil {
+		return fmt.Errorf("error waiting for deployment %q status to match expectation: %v", name, err)
+	}
+	return nil
+}
